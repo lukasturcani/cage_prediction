2bd505986788820f6e9e17a99a8dfe171c538c2b
Lukas Turcani <lukasturcani93@gmail.com>
2018-08-13 19:07:45 +0100

    almost done

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, current_app, request
+from flask import Flask, current_app, request, url_for, redirect
 import os
 from glob import iglob
 import pickle
@@ -23,23 +23,24 @@ def bb_count_fingerprint(mols, bits, radius):
         for bit, activators in info.items():
             fp[bit] = len(activators)
         full_fp.extend(fp)
+    full_fp.append(1)
     return full_fp
 
 
-def fingerprint(model_name):
+def fingerprint():
     bb = rdkit.AddHs(rdkit.MolFromSmiles(request.form['bb']))
     lk = rdkit.AddHs(rdkit.MolFromSmiles(request.form['lk']))
-    return bb_count_fingerprint((bb, lk), 512, 8)
+    return [bb_count_fingerprint((bb, lk), 512, 8)]
 
 
 def predict(model_name):
-    ans = current_app.config.models[model_name].predict(fingerprint())
-    return str(ans)
+    ans = current_app.models[model_name].predict(fingerprint())
+    return str(ans[0])
 
 
 def load_models():
     models = {}
-    for model_path in iglob('models/*'):
+    for model_path in iglob('backend/models/*.pkl'):
         name, _ = os.path.splitext(os.path.basename(model_path))
         with open(model_path, 'rb') as f:
             models[name] = pickle.load(f)
@@ -52,7 +53,12 @@ def create_app():
     if not os.path.exists(app.instance_path):
         os.mkdir(app.instance_path)
 
-    app.config.from_mapping(models=load_models())
+    app.models = load_models()
+
+    @app.route('/')
+    def root():
+        return redirect(url_for('static', filename='index.html'))
+
     app.route('/predict/<model_name>', methods=['POST'])(predict)
 
     return app
